ce5e7649a7ed712daaf9718df0b7f9b463a12366
GraceNam <zeidgame@gmail.com>
2021-03-02 22:53:06 +0900

    docs : 스프링 JPA 웹 개발
    + 스터디 모임 관리 서비스
      + 프로젝트 생성
      + 계정 도메인 작성

---
+++ src/main/java/com/studyolle/domain/Account.java
@@ -0,0 +1,56 @@
+package com.studyolle.domain;
+
+import lombok.*;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+@Entity
+@Getter @Setter @EqualsAndHashCode(of = "id")
+@Builder @AllArgsConstructor @NoArgsConstructor
+public class Account {
+
+    // 로그인
+    @Id @GeneratedValue
+    private Long id;
+
+    @Column(unique = true)
+    private String email;
+
+    // 프로필
+    @Column(unique = true)
+    private String nickname;
+
+    private String password;
+
+    private boolean emailVerified;
+
+    private String emailCheckToken;
+
+    private LocalDateTime joinedAt;
+
+    private String bio;
+
+    private String url;
+
+    private String occupation;
+
+    private String location;
+
+    @Lob @Basic(fetch = FetchType.EAGER)
+    private String profileImage;
+
+    // 알림
+    private boolean studyCreatedByEmail;
+
+    private boolean studyCreatedByWeb;
+
+    private boolean studyEnrollmentResultByEmail;
+
+    private boolean studyEnrollmentResultByWeb;
+
+    private boolean studyUpdatedByEmail;
+
+    private boolean studyUpdatedByWeb;
+
+}
